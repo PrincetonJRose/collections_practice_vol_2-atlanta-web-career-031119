9d6a108be2181c493785ef06605c742bf6c7059b
Princeton Rose <decoyzpr@gmail.com>
2019-03-09 15:58:51 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -47,7 +47,28 @@ def remove_non_strings(array)
 end
 
 def count_elements(element)
-
+  counts = []
+  counting = []
+  for item in element
+    if counting.include?(item)
+      next
+    else
+      counting << item
+    end
+  end
+  for item in counting
+    num = 0
+    for key in element
+      if item == key
+        num += 1
+      end
+    end
+    counts << num
+  end
+  num = -1
+  counting.each do |key|
+    key[:count] = counts[num += 1]
+  end
 end
 
 def merge_data(data1, data2)
